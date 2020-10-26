a65974a3c180786d75db6bdac5c9dbb090116b29
Alan Andrusky <alanandrusky@gmail.com>
2020-10-26 13:20:31 -0500

    alibi modules meets MVP

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 
-import { convictionSelect } from "./criminals/convictions/ConvictionSelect.js"
-import { criminalList } from "./criminals/CriminalList.js"
-import { officerList } from "./officers/OfficerList.js"
-import { OfficerSelect } from "./officers/OfficerSelect.js"
-// import { Criminal } from "./criminals/Criminal.js"
-import { NoteForm } from "./Notes/NoteForm.js"
-import { noteList} from "./Notes/NoteList.js"
+import { convictionSelect } from "./criminals/convictions/ConvictionSelect.js";
+import { criminalList } from "./criminals/CriminalList.js";
+import { officerList } from "./officers/OfficerList.js";
+import { OfficerSelect } from "./officers/OfficerSelect.js";
+import { NoteForm } from "./Notes/NoteForm.js";
+import { noteList} from "./Notes/NoteList.js";
+import { createAlibiEventListener } from "./criminals/ALibiList.js";
 
 NoteForm()
 criminalList()
 officerList()
 convictionSelect()
 OfficerSelect()
-// Criminal()
 noteList()
+
+createAlibiEventListener()
